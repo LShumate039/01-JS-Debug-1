50d8c2e19fdf0f7518ec8bea85bf3b0dd491686d
lennye Shumate <LShuamte039@west-mec.org>
2019-08-16 18:23:31 -0700

    update 8/16

--- sub-unit-01-DocumentObjectModel/photos.js
+++ sub-unit-01-DocumentObjectModel/photos.js
@@ -40,9 +40,110 @@ function leftArrow() {
 }
 
 /* open center figure in separate window */
-function zoomFig() {
-   
+
+
+function previewFive() {
+   var lastFigure = document.createElement("figure");
+   lastFigure.id = "fig5";
+   lastFigure.style.zIndex = "5";
+   lastFigure.style.position = "absolute";
+   lastFigure.style.right = "45px";
+   lastFigure.style.top = "67px";
+
+   var lastImage = document.createElement("img");
+   lastImage.width = "240";
+   lastImage.height = "135";
+
+   var articleElem = document.getElementsByTagName("article")[0];
+   // alert(articleElem);
+   lastFigure.appendChild(lastImage); 
+  // articleElem.(lastImage);
+   var firstFigure = lastFigure.cloneNode(true);
+firstFigure.id = "fig1";
+firstFigure.style.right = "";
+firstFigure.style.left = "45px";
+
+articleElem.appendChild(firstFigure); 
+
+
+   document.getElementsByTagName("img")[3].src = "images/IMG_0" + photoOrder[4] + "sm.jpg"; 
+   document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+   var figureCount = 3; 
+
+   articleElem.insertBefore(lastFigure, document.getElementById("rightarrow")); 
+
+   articleElem.insertBefore(firstFigure, document.getElementById("fig2"));
+
+document.getElementsByTagName("img")[0].src = "images/IMG_0" + "sm.jpg";
+
+document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
+
+var numberButton = document.querySelector("#fiveButton p");
+numberButton.innerHTML = "Show fewer images"; 
+
+if (numberButton.addEventListener) {
+   numberButton.removeEventListener("click", previewFive,
+   false);
+   numberButton.addEventListener("click", previewThree,
+   false);
+   }
+   else if (numberButton.attachEvent) {
+   numberButton.detachEvent("onclick", previewFive);
+   numberButton.attachEvent("onclick", previewThree);
+
+
+
+
 }
+
+
+
+
+function previewThree(){
+   var articleElem =
+document.getElementsByTagName("article")[0];
+var numberButton = document.querySelector(
+"#fiveButton");
+articleElem.removeChild(document.getElementById("fig1"));
+articleElem.removeChild(document.getElementById("fig5"));
+
+
+
+figureCount = 3;
+numberButton.innerHTML ="Show more images";
+if (numberButton.addEventListener) {
+numberButton.removeEventListener("click", previewthree,
+false);
+numberButton.addEventListener("click", previewFive,
+false);
+}
+else if (numberButton.attachEvent) {
+numberButton.detachEvent("onclick", previewThree);
+numberButton.attachEvent("onclick", previewFive);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+function zoomFig() {
+   var zoomWindow = window.open("zoom.html", "zoomwin",
+      "width=960,height=600");
+     
+}
+
+
 
 function createEventListeners(){
    var leftarrow = document.getElementById("leftarrow");
@@ -66,20 +167,8 @@ function createEventListeners(){
       showAllButton.attachEvent("onclick", previewFive);
    }
 
-   var articleElem = document.getElementsByTagName("article")[0];
-   alert(articleElem);
-   lastFigure.appendChild(lastImage); 
-   articleElem.appendChild(lastImage);
+   
 }
-var firstFigure = lastFigure.cloneNode(true);
-firstFigure.id = "fig1";
-firstFigure.style.right = "";
-firstFigure.style.left = "45px";
-
-articleElem.appendChild(firstFigure); 
-
-previewFive():
- document.getElementsByTagName("img")[3].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
 
 
 
@@ -92,16 +181,7 @@ previewFive():
 
 
 
-   var lastFigure = document.createElement("figure");
-   lastFigure.id = "fig5";
-   lastFigure.style.zIndex = "5";
-   lastFigure.style.position = "absolute";
-   lastFigure.style.right = "45px";
-   lastFigure.style.top = "67px";
-
-   var lastImage = document.createElement("img");
-   lastImage.width = "240";
-   lastImage.height = "135";
+   
 
 
 
@@ -132,17 +212,36 @@ function populateFigures(){
       currentFig = document.getElementsByTagName("img")[i - 1];
       currentFig.src = filename; 
    }
+if(figureCount === 3){
+   for(var i = 1; i < 4; i++){
+      filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+   }
+currentFig = document.getElementsByTagName("img")[i-1];
+currentFig.src = filename ; 
+
+}
+
+else{
+   for(var i = 0; i<5; i++){
+      filename = "image/IMG_0" + photoOrder[i] + "sm.jpg";
+      currentFig = document.getElementsByTagName("img")[i];
+      currentFig.src = filename; 
+      figureCount = 5; 
+   }
+}
+
 }
 
 
 /* create event listeners and populate image elements */
 function setUpPage() {
    createEventListeners();
+   populateFigures();
 }
 
 /* run setUpPage() function when page finishes loading */
 if (window.addEventListener) {
   window.addEventListener("load", setUpPage, false); 
-} else if (window.attachEvent)  {
+} else if (window.attachEvent) {
   window.attachEvent("onload", setUpPage);
 }
